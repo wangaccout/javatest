cc4f78eaf415a116cf55b7ea45d78ae4c88a0998
wangaccout <849416854@qq.com>
2022-06-29 18:09:46 +0800

    6.29

---
+++ maventest/mavenone/src/test/java/mavenone/NewTest.java
@@ -0,0 +1,13 @@
+package mavenone;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void f() {
+	  System.out.println("this is new test");
+	  Assert.assertTrue(true);
+  }
+}
